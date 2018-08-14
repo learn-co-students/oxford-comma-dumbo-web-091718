303ad5db39e0f80eeea60acc065a1eeb09d2eecf
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-14 04:57:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  else
+    list = ""
+    i = 0 
+    until i == array.length - 1
+      list << "#{array[i]}, "
+      i+=1
+    end
+    list << "and #{array[i]}"
+  end
 end
