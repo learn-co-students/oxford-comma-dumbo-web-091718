723364c7262c6d76c2803ed249501d1cf1cdd93d
milanla <milan.lanlada@gmail.com>
2018-08-21 23:59:45 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    str = ""
+    i = 0
+    until i == array.size - 1
+      str << "#{array[i]}, "
+      i += 1
+    end
+    str << "and #{array[i]}"
+  end
 end
