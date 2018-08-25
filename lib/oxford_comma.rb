7319369dba25b93bea01b0810d9125e27645d715
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-25 16:56:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  big_array = []
+  and_last_object = "and #{array.last}"
+  if array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    big_array << array
+    big_array.join(', ')
+  else
+    array.join(' and ')
+  end
 end
