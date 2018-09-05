661c969033cfea077ee6da237ac262c12806976a
soohoo05 <tyler.soohoo@gmail.com>
2018-09-05 12:32:08 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+if array.count == 1
+  return array[0]
+elsif array.count == 2
+  return array.join(" and ")
+elsif array.count == 3
+  array[-1].prepend "and "
+  return array.join(", ")
+elsif array.count >3
+  array[-1].prepend "and "
+	array.join(", ")
+  
+end
 end
