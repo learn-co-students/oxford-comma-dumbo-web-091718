60013a19ae418d322eded1e63ab022e0f8f1035c
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-09 18:28:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    listed = []
+    array.each do |x|
+      listed << x
+      listed << ", "
+    end
+    listed.pop
+    listed.delete_at(-2)
+    listed.insert(-2, ", and ")
+    listed.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.join
+  end
 end
