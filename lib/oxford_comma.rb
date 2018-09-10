4cb1afc9e3a9fd76eb984a3e0083f3ae493f03b9
Nasser Sanou <nassersanou23@gmail.com>
2018-09-10 17:03:52 +0000

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
+array = ["kiwi", "durian"]
+# format_array =  array[0] + ", " + array[1] + ", " + "and" + " " + array[-1]
 
+def oxford_comma(array)
+  
+if array.length == 1 
+  return array.join()
+end 
+
+ if  array.length == 2
+   return array.join(" " + 'and'+ " " )
+ end 
+ 
+if array.length == 3 
+  return array[0..-2].join(', ') + array[-1].insert(0, ', and ')
+  
+end 
+  
+if array.length > 3 
+   return array[0..-2].join(', ') + array[-1].insert(0, ', and ')
+end 
+ array
 end
+
+p oxford_comma(array)
